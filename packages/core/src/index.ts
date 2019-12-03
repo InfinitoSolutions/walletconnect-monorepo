254a2dc377604c9852f6dd05141f3da16d12cb31
Kha Nguyen <mr.kha63@gmail.com>
2019-12-03 14:28:10 +0700

    Revert ver

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -563,8 +563,8 @@ class Connector implements IConnector {
 
   /**
    * IW send transaction
-   * @param platform 
-   * @param params 
+   * @param platform
+   * @param params
    */
   public async iwSendTransaction (platform: string ,params: any) {
     if (!this._connected) {
@@ -589,8 +589,8 @@ class Connector implements IConnector {
 
   /**
    * IW get wallet address by platform
-   * @param platform 
-   * @param options 
+   * @param platform
+   * @param options
    */
   public async iwGetAccounts (platform: string , options: any) {
     if (!this._connected) {
@@ -614,8 +614,8 @@ class Connector implements IConnector {
 
   /**
    * IW sign message by platform
-   * @param platform 
-   * @param data 
+   * @param platform
+   * @param data
    */
   public async iwSignMessage (platform: string, data: any) {
     if (!this._connected) {
@@ -638,10 +638,10 @@ class Connector implements IConnector {
     }
   }
 
-    /**
+  /**
    * IW Send Raw by platform
-   * @param platform 
-   * @param data 
+   * @param platform
+   * @param data
    */
   public async iwSendRaw (platform: string, data: any) {
     if (!this._connected) {
@@ -650,6 +650,33 @@ class Connector implements IConnector {
 
     const request = this._formatRequest({
       method: 'iw_sendRaw',
+      params: [{
+        platform,
+        params: data
+      }]
+    })
+
+    try {
+      const result = await this._sendCallRequest(request)
+      return result
+    } catch (error) {
+      throw error
+    }
+  }
+
+  /**
+   * IW invoke custom function
+   * @param method
+   * @param platform
+   * @param data
+   */
+  public async iwCustomFunction (method: string, platform: string, data: any) {
+    if (!this._connected) {
+      throw new Error(ERROR_SESSION_DISCONNECTED)
+    }
+
+    const request = this._formatRequest({
+      method,
       params: [{
         platform,
         params: data
